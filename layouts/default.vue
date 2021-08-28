29ae5ce5060033cfb47f83f6fbadab4d8bae247a
francisgill42 <francisgill42@gmail.com>
2021-08-28 13:59:59 -0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+     class="info"
+      dark
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+     
+      <v-toolbar-title  v-text="title" />
+      <v-spacer />
+      <v-btn
+        icon
+        @click="logout"
+      >
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'User',
+          to: '/User'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Admin Panel'
+    }
+  },
+  methods:{
+  async logout() {
+    await this.$auth.logout();
+  },
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,96 @@
+<template>
+<v-app>
+   
+<v-main>
+<v-container
+class="fill-height"
+>
+<v-row
+justify="center"
+>
+<v-col
+cols="12"
+sm="8"
+md="6"
+>
+<v-card class="elevation-12">
+
+  
+     
+<v-toolbar color="" dark flat>
+<v-toolbar-title class="white--text">Login form</v-toolbar-title>
+</v-toolbar>
+<v-card-text>
+   <v-form
+    method="post"
+    ref="form"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="email"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      label="Password"
+      required
+      type="password"
+    ></v-text-field>
+
+  
+    <v-btn color="blue" :loading="loading"  @click="login">Login</v-btn>
+
+  </v-form>
+</v-card-text>
+<v-card-actions>
+</v-card-actions>
+</v-card>
+</v-col>
+</v-row>
+</v-container>
+</v-main>
+</v-app>
+</template>
+
+<script>
+
+
+export default {
+layout:'login',
+data:() => ({
+loading:false,    
+snackbar:false,    
+email:'',
+password:'',          
+msg:''
+}),
+methods:{
+
+login () {
+this.loading = true
+
+let credentials = {
+email: this.email,
+password: this.password
+};
+this.$auth.loginWith('local',{ data:credentials })
+.catch((e) => {
+    this.snackbar = true
+    this.msg = e.response.data.error 
+    setTimeout(() => {
+        this.loading = false
+        // location.reload();
+    },3000);
+})
+
+
+}
+
+
+
+}
+
+}
+</script>
